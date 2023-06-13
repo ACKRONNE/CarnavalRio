567dcaf1ecc848fc7c812c7a00b4ab055879d2e1
ACKRONNE <ACKRONNE@GMAIL.COM>
2023-06-12 22:02:54 -0400

    Create and Alter modifications

--- 3. CreateTable.sql
+++ 3. CreateTable.sql
@@ -1,6 +1,6 @@
 -- Crear Tablas
 
-CREATE TABLE clientes (
+CREATE TABLE ama_clientes (
 	id_cliente		integer	,
 	nombre			varchar(10) 	NOT NULL,
 	apellido1		varchar(10)		NOT NULL,
@@ -10,45 +10,47 @@ CREATE TABLE clientes (
 	dni				integer			NOT NULL,
 	correo			varchar(30)		NOT NULL,
 	PRIMARY KEY		(id_cliente)		
-	);
+);
 
-CREATE TABLE lugares_evento (
+CREATE TABLE ama_lugares_evento (
 	id_lugar		integer,
 	nombre			varchar(10) 	NOT NULL,
 	direccion		text			NOT NULL,
 	PRIMARY KEY		(id_lugar)
-	);
+);
 
-CREATE TABLE regiones_rio (
+CREATE TABLE ama_regiones_rio (
 	id_region		integer,
 	nombre			varchar(10) 	NOT NULL,
 	descripcion		text,
 	PRIMARY KEY		(id_region)
 );
 
-CREATE TABLE empresas (
+CREATE TABLE ama_empresas (
 	id_empresa		integer,
 	nombre			varchar(10) 	NOT NULL,
 	correo			varchar(30)		NOT NULL,
 	PRIMARY KEY		(id_empresa)
 );
 
-CREATE TABLE autorizaciones (
+CREATE TABLE ama_autorizaciones (
 	id_empresa 		integer,
-	cant_max		integer			NOT NULL
+	cant_max		integer			NOT NULL,
+	PRIMARY KEY		(id_empresa)
 );
 
-CREATE TABLE tipos_entrada (
+CREATE TABLE ama_tipos_entrada (
 	id_empresa		integer,
-	id_tipo			integer			NOT NULL,
+	id_tipo			integer,
 	tipo_ent		char(2)			NOT NULL,
 	sector			integer			NOT NULL,
 	calidad			integer			NOT NULL,
 	tipo_des		char(1)			NOT NULL,
-	ubi				char(2)			
+	ubi				char(2),
+	PRIMARY KEY (id_empresa, id_tipo);	
 );
 
-CREATE TABLE reserva (
+CREATE TABLE ama_reserva (
 	id_reserva		integer,
 	f_h_emi			timestamp		NOT NULL,
 	id_cliente		integer			NOT NULL,
@@ -57,7 +59,7 @@ CREATE TABLE reserva (
 	PRIMARY KEY		(id_reserva)		
 );
 
-CREATE TABLE escuelas_samba (
+CREATE TABLE ama_escuelas_samba (
 	id_escuela 		integer,
 	nombre_gres		varchar(30)		NOT NULL,
 	fecha_funda		date			NOT NULL,
@@ -67,14 +69,15 @@ CREATE TABLE escuelas_samba (
 	PRIMARY KEY		(id_escuela)
 );
 
-CREATE TABLE hist_grupo (
+CREATE TABLE ama_hist_grupo (
 	id_escuela		integer,
-	fecha_ini		integer			NOT NULL,
+	fecha_ini		date,
 	grupo			char(1)			NOT NULL,
-	fecha_fin		date
+	fecha_fin		date,
+	PRIMARY KEY		(id_escuela,fecha_ini)
 );
 
-CREATE TABLE protagonistas (
+CREATE TABLE ama_protagonistas (
 	id_prota		integer,
 	nombre			varchar(10)		NOT NULL,
 	apellido1		varchar(10)		NOT NULL,
@@ -82,10 +85,11 @@ CREATE TABLE protagonistas (
 	genero			char(1)			NOT NULL,
 	fecha_nac		date			NOT NULL,
 	dni				integer			NOT NULL,
-	id_escuela		integer
+	id_escuela		integer,
+	PRIMARY KEY		(id_prota)
 );
 
-CREATE TABLE carnavales_anual (
+CREATE TABLE ama_carnavales_anual (
 	ano				date,
 	fecha_ini		date			NOT NULL,
 	fecha_fin		date			NOT NULL,
@@ -93,9 +97,9 @@ CREATE TABLE carnavales_anual (
 	PRIMARY KEY		(ano)
 );
 
-CREATE TABLE eventos (
-	ano 			date			NOT NULL,
-	id_evento		integer			NOT NULL,
+CREATE TABLE ama_eventos (
+	ano 			date,
+	id_evento		integer,
 	tipo			char(1)			NOT NULL,
 	fecha_ini		date			NOT NULL,
 	hora_ini		time			NOT NULL,
@@ -103,50 +107,56 @@ CREATE TABLE eventos (
 	pago			char(1)			NOT NULL,
 	descripcion		text,
 	costo_reales	integer,
-	id_lugar		integer
+	id_lugar		integer,
+	PRIMARY KEY (ano, id_evento)
 );
 
-CREATE TABLE participacion (
-	id_escuela		integer			NOT NULL,
-	fecha_ini		date			NOT NULL,
-	id_evento		integer			NOT NULL,
+CREATE TABLE ama_participacion (
+	id_escuela		integer,
+	fecha_ini		date,
+	id_evento		integer,
 	hora_ini		time			NOT NULL,
 	orden_des		integer			NOT NULL,
 	posicion_fin	integer			NOT NULL,
 	tema_gen		varchar(20)		NOT NULL,
-	titu_letr		text			NOT NULL
+	titu_letr		text			NOT NULL,
+	PRIMARY KEY (id_escuela, fecha_ini, id_evento)
 );
 
-CREATE TABLE rol (
-	id_prota		integer			NOT NULL,
-	id_escuela		integer			NOT NULL,
-	fecha_ini		date			NOT NULL,
-	id_evento		integer			NOT NULL,
-	nombre			varchar(1)		NOT NULL
+CREATE TABLE ama_rol (
+	id_prota		integer,
+	id_escuela		integer,
+	fecha_ini		date,
+	id_evento		integer,
+	nombre			varchar(1)		NOT NULL,
+	PRIMARY KEY (id_prota, id_escuela, fecha_ini,id_evento)
 );
 
-CREATE TABLE detalle_reserva (
-	id_reserva		integer			NOT NULL,
-	id_empresa		integer			NOT NULL,
-	cantidad		integer			NOT NULL
+CREATE TABLE ama_detalle_reserva (
+	id_reserva		integer,
+	id_empresa		integer,
+	cantidad		integer			NOT NULL,
+	PRIMARY KEY (id_reserva, id_empresa)
 );
 
-CREATE TABLE entrada (
-	ano				date			NOT NULL,
-	id_evento		integer			NOT NULL,
-	id_entrada		integer			NOT NULL,
+CREATE TABLE ama_entrada (
+	ano				date,
+	id_evento		integer,
+	id_entrada		integer,
 	hora_emi		time			NOT NULL,
 	f_emision		date			NOT NULL,
-	id_reserva		integer			NOT NULL
+	id_reserva		integer			NOT NULL,
+	PRIMARY KEY (ano, id_evento, id_entrada)
 );
 
-CREATE TABLE historico_precio (
-	id_empresa		integer			NOT NULL,
-	id_tipo			integer			NOT NULL,
-	ano	   			date			NOT NULL,
-	id_evento	    integer			NOT NULL,
-	id_entrada	   	integer			NOT NULL,
-	fecha_inicio	date 			NOT NULL,
+CREATE TABLE ama_historico_precio (
+	id_empresa		integer,
+	id_tipo			integer,
+	ano	   			date,
+	id_evento	    integer,
+	id_entrada	   	integer,
+	fecha_inicio	date,
 	costo_reales	integer			NOT NULL,
-	fecha_fin		date
+	fecha_fin		date,
+	PRIMARY KEY (id_empresa, id_tipo, ano, id_evento, id_entrada, fecha_inicio)
 );
--- 4. AlterTable.sql
+++ 4. AlterTable.sql
@@ -10,84 +10,110 @@
 -- pkt | foreing key en tabla tipos_entrada
 -- ukr | unique en tabla reserva
 -- fkr | foreing key en tabla reserva
+-- fkes| foreing key en tabla escuelas
+-- ukes| unique en tabla escuelas
+-- fkh 
+-- ckh
+-- fkp
+-- ckp
+-- ukp
+-- ukp
+-- fkca
+-- fkev
+-- fkev
+-- ckev
+-- ckev
+-- ukp
+-- fkpa
+-- fkpa
+-- fkpa
+-- fkro
+-- fkro
+-- fkro
+-- fkro
+-- ckro
+-- fkde
+-- fkde
+-- fken
+-- fken
+-- fkde
+-- uken
+-- fkhp
+-- fkhp
+-- fken
+-- fkhp
+-- fkhp
 
-ALTER TABLE clientes
+ALTER TABLE ama_clientes
 ADD CONSTRAINT ckc_naciona      CHECK (nacionalidad IN ('n', 'e')),
 ADD CONSTRAINT ukc_dni		    UNIQUE (dni),
 ADD CONSTRAINT ukc_correo	    UNIQUE (correo);
 
-ALTER TABLE empresas
+ALTER TABLE ama_empresas
 ADD CONSTRAINT uke_correo       UNIQUE (correo);
 
-ALTER TABLE autorizaciones
+ALTER TABLE ama_autorizaciones
 ADD CONSTRAINT  fka_auto_emp    FOREIGN KEY     (id_empresa)    REFERENCES  empresas (id_empresa);
 
-ALTER TABLE tipos_entrada
+ALTER TABLE ama_tipos_entrada
+ADD CONSTRAINT fkt_emp_tip      FOREIGN KEY     (id_empresa)    REFERENCES  empresas (id_empresa),
 ADD CONSTRAINT ckt_tipoEnt      CHECK (tipo_ent IN ('gp','gf','an','sl')),
 ADD CONSTRAINT ckt_sector       CHECK ((sector >= 1) AND (sector <= 11)),
 ADD CONSTRAINT ckt_calidad      CHECK ((calidad >= 1) AND (calidad <= 11)),
 ADD CONSTRAINT ckt_tipDesfile   CHECK (tipo_des IN ('e', 'c', 'a')),
-ADD CONSTRAINT ckt_ubi          CHECK (ubi IN ('a', 'b','c', 'a/b', 'c/d')),
-ADD CONSTRAINT pkt_tipos_ent    PRIMARY KEY (id_empresa, id_tipo);
+ADD CONSTRAINT ckt_ubi          CHECK (ubi IN ('a', 'b','c', 'a/b', 'c/d'));
 
-ALTER TABLE reserva
-ADD CONSTRAINT ukr_idcli        UNIQUE (id_cliente),
-ADD CONSTRAINT fkr_reser        FOREIGN KEY    (id_cliente)     REFERENCES clientes(id_cliente);
+ALTER TABLE ama_reserva
+ADD CONSTRAINT fkr_reser        FOREIGN KEY    (id_cliente)     REFERENCES clientes(id_cliente),
+ADD CONSTRAINT ukr_idcli        UNIQUE (id_cliente);
 
-ALTER TABLE escuelas_samba
-ADD CONSTRAINT ukes_idreg       UNIQUE (id_region),
-ADD CONSTRAINT fkes_escu        FOREIGN KEY    (id_region)      REFERENCES regiones_rio(id_region);
+ALTER TABLE ama_escuelas_samba
+ADD CONSTRAINT fkes_escu        FOREIGN KEY    (id_region)      REFERENCES regiones_rio(id_region),
+ADD CONSTRAINT ukes_idreg       UNIQUE (id_region);
 
-ALTER TABLE hist_grupo
-ADD CONSTRAINT ckh_grupo        CHECK (grupo IN ('a','e')),
-ADD CONSTRAINT pkh_histgr       PRIMARY KEY (id_escuela, fecha_ini),
-ADD CONSTRAINT fkh_histgr       FOREIGN KEY    (id_escuela)     REFERENCES escuelas_samba(id_escuela);
+ALTER TABLE ama_hist_grupo
+ADD CONSTRAINT fkh_idescu       FOREIGN KEY    (id_escuela)     REFERENCES escuelas_samba(id_escuela),
+ADD CONSTRAINT ckh_grupo        CHECK (grupo IN ('a','e'));
 
-ALTER TABLE protagonistas
+ALTER TABLE ama_protagonistas
+ADD CONSTRAINT fkp_idesc        FOREIGN KEY    (id_escuela)     REFERENCES escuelas_samba(id_escuela),
 ADD CONSTRAINT ckp_gen          CHECK (genero IN ('f','m')),
 ADD CONSTRAINT ukp_dni          UNIQUE (dni),
-ADD CONSTRAINT ukp_idesc        UNIQUE (id_escuela),
-ADD CONSTRAINT fkp_idesc        FOREIGN KEY    (id_escuela)     REFERENCES escuelas_samba(id_escuela);
+ADD CONSTRAINT ukp_idesc        UNIQUE (id_escuela);
 
--- Aqui falta decir que el momo va a ser el hombre y reina la mujer creo que va a dar error de ejecucion
-ALTER TABLE carnavales_anual
-ADD CONSTRAINT fkca_momo        FOREIGN KEY    (id_prota)      REFERENCES protagonistas(id_prota);
+ALTER TABLE ama_carnavales_anual
+ADD CONSTRAINT fkca_prota       FOREIGN KEY    (id_prota)      REFERENCES protagonistas(id_prota);
 
-ALTER TABLE eventos
+ALTER TABLE ama_eventos
+ADD CONSTRAINT fkev_ano         FOREIGN KEY (ano)              REFERENCES carnavales_anual(ano),
+ADD CONSTRAINT fkev_lug         FOREIGN KEY (id_lugar)         REFERENCES lugares_evento(id_lugar),
 ADD CONSTRAINT ckev_tipo        CHECK (tipo IN ('g','d')),
 ADD CONSTRAINT ckev_pago        CHECK (pago IN ('s','n')),
-ADD CONSTRAINT pkev_eve         PRIMARY KEY (ano, id_evento),
-ADD CONSTRAINT fkev_ano         FOREIGN KEY (ano)           REFERENCES carnavales_anual(ano),
-ADD CONSTRAINT fkev_lug         FOREIGN KEY (id_lugar)      REFERENCES lugares_evento(id_lugar);
+ADD CONSTRAINT ukp_dni          UNIQUE (id_lugar);
 
-ALTER TABLE participacion
-ADD CONSTRAINT pkpa_part        PRIMARY KEY (id_escuela, fecha_ini, id_evento),
-ADD CONSTRAINT fkpa_idesc       FOREIGN KEY (id_escuela)     REFERENCES escuelas_samba(id_escuela),
-ADD CONSTRAINT fkpa_fini        FOREIGN KEY (fecha_ini)      REFERENCES hist_grupo(fecha_ini),
-ADD CONSTRAINT fkpa_idev        FOREIGN KEY (id_evento)      REFERENCES eventos(id_evento);
+ALTER TABLE ama_participacion
+ADD CONSTRAINT fkpa_idesc       FOREIGN KEY (id_escuela)       REFERENCES escuelas_samba(id_escuela),
+ADD CONSTRAINT fkpa_fini        FOREIGN KEY (fecha_ini)        REFERENCES hist_grupo(fecha_ini),
+ADD CONSTRAINT fkpa_idev        FOREIGN KEY (id_evento)        REFERENCES eventos(id_evento);
 
-ALTER TABLE rol
-ADD CONSTRAINT pkro_rol         PRIMARY KEY (id_prota, id_escuela, fecha_ini,id_evento),
-ADD CONSTRAINT fkro_idpro       FOREIGN KEY (id_prota)       REFERENCES protagonistas(id_prota),
-ADD CONSTRAINT fkro_idesc       FOREIGN KEY (id_escuela)     REFERENCES escuelas_samba(id_escuela),
-ADD CONSTRAINT fkro_fini        FOREIGN KEY (fecha_ini)      REFERENCES hist_grupo(fecha_ini),
-ADD CONSTRAINT fkro_ideve       FOREIGN KEY (id_evento)      REFERENCES eventos(id_evento),
+ALTER TABLE ama_rol
+ADD CONSTRAINT fkro_idpro       FOREIGN KEY (id_prota)         REFERENCES protagonistas(id_prota),
+ADD CONSTRAINT fkro_idesc       FOREIGN KEY (id_escuela)       REFERENCES escuelas_samba(id_escuela),
+ADD CONSTRAINT fkro_fini        FOREIGN KEY (fecha_ini)        REFERENCES hist_grupo(fecha_ini),
+ADD CONSTRAINT fkro_ideve       FOREIGN KEY (id_evento)        REFERENCES eventos(id_evento),
 ADD CONSTRAINT ckro_name        CHECK (nombre IN ('c','m','p'));
 
-ALTER TABLE detalle_reserva
-ADD CONSTRAINT pkde_res         PRIMARY KEY (id_reserva, id_empresa),
-ADD CONSTRAINT fkde_idres       FOREIGN KEY (id_reserva)      REFERENCES reserva(id_reserva),
-ADD CONSTRAINT fkde_idemp       FOREIGN KEY (id_empresa)      REFERENCES empresa(id_empresa);
+ALTER TABLE ama_detalle_reserva
+ADD CONSTRAINT fkde_idres       FOREIGN KEY (id_reserva)       REFERENCES reserva(id_reserva),
+ADD CONSTRAINT fkde_idemp       FOREIGN KEY (id_empresa)       REFERENCES empresa(id_empresa);
 
-ALTER TABLE entrada
-ADD CONSTRAINT uken_idres        UNIQUE (id_reserva),
-ADD CONSTRAINT pken_entra        PRIMARY KEY (ano, id_evento, id_entrada),
-ADD CONSTRAINT fken_ano          FOREIGN KEY (ano)            REFERENCES carnavales_anual(ano),
-ADD CONSTRAINT fken_ideve        FOREIGN KEY (id_evento)      REFERENCES eventos(id_evento),
-ADD CONSTRAINT fkde_idres        FOREIGN KEY (id_reserva)     REFERENCES reserva(id_reserva);
+ALTER TABLE ama_entrada
+ADD CONSTRAINT fken_ano          FOREIGN KEY (ano)             REFERENCES carnavales_anual(ano),
+ADD CONSTRAINT fken_ideve        FOREIGN KEY (id_evento)       REFERENCES eventos(id_evento),
+ADD CONSTRAINT fkde_idres        FOREIGN KEY (id_reserva)      REFERENCES reserva(id_reserva),
+ADD CONSTRAINT uken_idres        UNIQUE      (id_reserva);
 
-ALTER TABLE historico_precio
-ADD CONSTRAINT pkhp_histpre      PRIMARY KEY (id_empresa, id_tipo, ano, id_evento, id_entrada, fecha_inicio),
+ALTER TABLE ama_historico_precio
 ADD CONSTRAINT fkhp_idemp        FOREIGN KEY (id_empresa)      REFERENCES empresa(id_empresa),
 ADD CONSTRAINT fkhp_idtip        FOREIGN KEY (id_tipo)         REFERENCES tipos_entrada(id_tipo),
 ADD CONSTRAINT fken_ano          FOREIGN KEY (ano)             REFERENCES carnavales_anual(ano),
