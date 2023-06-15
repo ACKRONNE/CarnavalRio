f667d029fbfec2776b87e772df6386be24111d13
ACKRONNE <ACKRONNE@GMAIL.COM>
2023-06-14 20:50:42 -0400

    SERIAL fixed

--- 3. CreateTable.sql
+++ 3. CreateTable.sql
@@ -1,7 +1,7 @@
 -- Crear Tablas
 
 CREATE TABLE ama_clientes (
-	id_cliente		integer							SERIAL,
+	id_cliente		SERIAL,
 	nombre			varchar(10) 	NOT NULL,
 	apellido1		varchar(10)		NOT NULL,
 	apellido2		varchar(10)		NOT NULL,
@@ -13,35 +13,35 @@ CREATE TABLE ama_clientes (
 );
 
 CREATE TABLE ama_lugares_evento (
-	id_lugar		integer							SERIAL,
+	id_lugar		SERIAL,
 	nombre			varchar(10) 	NOT NULL,
 	direccion		text			NOT NULL,
 	PRIMARY KEY		(id_lugar)
 );
 
 CREATE TABLE ama_regiones_rio (
-	id_region		integer							SERIAL,
+	id_region		SERIAL,
 	nombre			varchar(10) 	NOT NULL,
 	descripcion		text,
 	PRIMARY KEY		(id_region)
 );
 
 CREATE TABLE ama_empresas (
-	id_empresa		integer							SERIAL,
+	id_empresa		SERIAL,
 	nombre			varchar(10) 	NOT NULL,
 	correo			varchar(30)		NOT NULL		UNIQUE,
 	PRIMARY KEY		(id_empresa)
 );
 
 CREATE TABLE ama_autorizaciones (
-	id_empresa 		integer							SERIAL,
+	id_empresa 		SERIAL,
 	cant_max		integer			NOT NULL,
 	PRIMARY KEY		(id_empresa)
 );
 
 CREATE TABLE ama_tipos_entrada (
-	id_empresa		integer							SERIAL,
-	id_tipo			integer							SERIAL			UNIQUE,
+	id_empresa		SERIAL,
+	id_tipo			SERIAL			UNIQUE,
 	tipo_ent		char(2)			NOT NULL		CHECK (tipo_ent IN ('gp','gf','an','sl')),
 	sector			integer			NOT NULL		CHECK ((sector >= 1) AND (sector <= 11)),
 	calidad			integer			NOT NULL		CHECK ((calidad >= 1) AND (calidad <= 11)),
@@ -51,7 +51,7 @@ CREATE TABLE ama_tipos_entrada (
 );
 
 CREATE TABLE ama_reserva (
-	id_reserva		integer							SERIAL,
+	id_reserva		SERIAL,
 	f_h_emi			timestamp		NOT NULL,
 	id_cliente		integer			NOT NULL		UNIQUE,
 	monto_reales	integer,
@@ -60,7 +60,7 @@ CREATE TABLE ama_reserva (
 );
 
 CREATE TABLE ama_escuelas_samba (
-	id_escuela 		integer							SERIAL,
+	id_escuela 		SERIAL,
 	nombre_gres		varchar(30)		NOT NULL,
 	fecha_funda		date			NOT NULL,
 	direccion		text			NOT NULL,
@@ -70,7 +70,7 @@ CREATE TABLE ama_escuelas_samba (
 );
 
 CREATE TABLE ama_hist_grupo (
-	id_escuela		integer							SERIAL,
+	id_escuela		SERIAL,
 	fecha_ini		date							UNIQUE,
 	grupo			char(1)			NOT NULL		CHECK (grupo IN ('a','e')),
 	fecha_fin		date,
@@ -78,7 +78,7 @@ CREATE TABLE ama_hist_grupo (
 );
 
 CREATE TABLE ama_protagonistas (
-	id_prota		integer							SERIAL,
+	id_prota		SERIAL,
 	nombre			varchar(10)		NOT NULL,
 	apellido1		varchar(10)		NOT NULL,
 	apellido2		varchar(10)		NOT NULL,
@@ -99,7 +99,7 @@ CREATE TABLE ama_carnavales_anual (
 
 CREATE TABLE ama_eventos (
 	ano 			date,
-	id_evento		integer							SERIAL			UNIQUE,
+	id_evento		SERIAL			UNIQUE,
 	tipo			char(1)			NOT NULL,
 	fecha_ini		date			NOT NULL,
 	hora_ini		time			NOT NULL,					
@@ -112,9 +112,9 @@ CREATE TABLE ama_eventos (
 );
 
 CREATE TABLE ama_participacion (
-	id_escuela		integer							SERIAL,
+	id_escuela		SERIAL,
 	fecha_ini		date,
-	id_evento		integer							SERIAL,
+	id_evento		SERIAL,
 	hora_ini		time			NOT NULL,
 	orden_des		integer			NOT NULL,
 	posicion_fin	integer			NOT NULL,
@@ -124,37 +124,37 @@ CREATE TABLE ama_participacion (
 );
 
 CREATE TABLE ama_rol (
-	id_prota		integer							SERIAL,
-	id_escuela		integer							SERIAL,
+	id_prota		SERIAL,
+	id_escuela		SERIAL,
 	fecha_ini		date,
-	id_evento		integer							SERIAL			UNIQUE,
+	id_evento		SERIAL			UNIQUE,
 	nombre			varchar(1)		NOT NULL		CHECK (nombre IN ('c','m','p')),
 	PRIMARY KEY (id_prota, id_escuela, fecha_ini,id_evento)
 );
 
 CREATE TABLE ama_detalle_reserva (
-	id_reserva		integer							SERIAL,
-	id_empresa		integer							SERIAL,
+	id_reserva		SERIAL,
+	id_empresa		SERIAL,
 	cantidad		integer			NOT NULL,
 	PRIMARY KEY (id_reserva, id_empresa)
 );
 
 CREATE TABLE ama_entrada (
 	ano				date,
-	id_evento		integer							SERIAL,
-	id_entrada		integer							SERIAL			UNIQUE,
+	id_evento		SERIAL,
+	id_entrada		SERIAL			UNIQUE,
 	hora_emi		time			NOT NULL,
 	f_emision		date			NOT NULL,
-	id_reserva		integer							SERIAL			UNIQUE,
+	id_reserva		SERIAL			UNIQUE,
 	PRIMARY KEY (ano, id_evento, id_entrada)
 );
 
 CREATE TABLE ama_historico_precio (
-	id_empresa		integer							SERIAL,
-	id_tipo			integer							SERIAL,
+	id_empresa		SERIAL,
+	id_tipo			SERIAL,
 	ano	   			date,
-	id_evento	    integer							SERIAL,
-	id_entrada	   	integer							SERIAL,
+	id_evento	    SERIAL,
+	id_entrada	   	SERIAL,
 	fecha_inicio	date,
 	costo_reales	integer			NOT NULL,
 	fecha_fin		date,
