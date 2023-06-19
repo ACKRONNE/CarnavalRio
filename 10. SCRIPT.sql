5bd0943c9b91738374537e51167341e74b8d72d8
ACKRONNE <ACKRONNE@GMAIL.COM>
2023-06-18 21:43:31 -0400

    in_carn fixed

--- 10. SCRIPT.sql
+++ 10. SCRIPT.sql
@@ -229,11 +229,10 @@ ADD CONSTRAINT fkhp_ident       FOREIGN KEY (id_entrada)        REFERENCES ama_e
 CREATE INDEX in_rese ON ama_reservas (id_cliente);
 CREATE INDEX in_escu ON ama_escuelas_samba (id_region);
 CREATE INDEX in_prot ON ama_protagonistas (id_escuela);
-CREATE INDEX in_carn ON ama_carnavales_anuales (id_momo);
-CREATE INDEX in_carn ON ama_carnavales_anuales (id_reina);
+CREATE INDEX in_momo ON ama_carnavales_anuales (id_momo);
+CREATE INDEX in_rein ON ama_carnavales_anuales (id_reina);
 CREATE INDEX in_even ON ama_eventos (id_lugar);
 CREATE INDEX in_entr ON ama_entradas (id_reservas);
-
 -- I N S E R C C I O N  D E  D A T O S
 
 BEGIN;
--- 7. Indices.sql
+++ 7. Indices.sql
@@ -3,7 +3,7 @@
 CREATE INDEX in_rese ON ama_reservas (id_cliente);
 CREATE INDEX in_escu ON ama_escuelas_samba (id_region);
 CREATE INDEX in_prot ON ama_protagonistas (id_escuela);
-CREATE INDEX in_carn ON ama_carnavales_anuales (id_momo);
-CREATE INDEX in_carn ON ama_carnavales_anuales (id_reina);
+CREATE INDEX in_momo ON ama_carnavales_anuales (id_momo);
+CREATE INDEX in_rein ON ama_carnavales_anuales (id_reina);
 CREATE INDEX in_even ON ama_eventos (id_lugar);
 CREATE INDEX in_entr ON ama_entradas (id_reservas);
